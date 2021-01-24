42c16784a044c1338eb535d8bd05eb9c1d0d6ed3
Richie Goulazian <rgoulazian@gmail.com>
2021-01-24 11:53:23 -0500

    Create basic homepage

---
+++ layouts/BaseLayout.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function BaseLayout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>{title}</title>
+      </Head>
+      {children}
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import BaseLayout from '../layouts/BaseLayout'
 
 export default function Home() {
   return (
-    <div>
-    </div>
-  )
+    <BaseLayout title="Richie Goulazian - Portfolio">
+      <div className="container">
+        <div className="mt-5 d-flex flex-column">
+          <img id="photo" className="rounded-circle d-block mx-auto" src="/photo.jpg" />
+          <h1 id="name" className="display-4 mt-4 text-center">Richie Goulazian</h1>
+        </div>
+      </div>
+
+      <style jsx>{`
+        #photo {
+          width: 150px;
+        }
+
+        #name {
+          font-family: Oswald, sans-serif;
+        }
+      `}</style>
+    </BaseLayout>
+  );
 }
