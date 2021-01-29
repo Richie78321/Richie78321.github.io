04ddf681eef06186230938980c59ddb3636c33e8
Richie Goulazian <rgoulazian@gmail.com>
2021-01-29 12:53:03 -0500

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/*Google Analytics*/}
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-180638861-1"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'UA-180638861-1');
+              `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
